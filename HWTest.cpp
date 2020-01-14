0d52cdb5e8ebffa028cefeeb2066553bc9ee3287
kahnbenjamina <bkahn@chapman.edu>
2020-01-14 20:53:50 +0000

    Added some lines. Testing committing changes.

--- HWTest.cpp
+++ HWTest.cpp
@@ -4,6 +4,8 @@ using namespace std;
 int main()
 {
   cout << "Hello World!" << endl;
+  cout << "I had to add this statement because Rene said so." << endl;
+  cout << "adding another cout.";
 
   return 0;
 }
